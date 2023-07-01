63524aea349b53dbe0bcc9e5bb150d4fc82c53c2
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2023-07-01 23:52:21 +0500

    +3

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -37,7 +37,7 @@ module.exports = {
       repo: 'git@github.com:Zorkiy82/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'mkdir ~/mesto-backend && scp ./backend/* ~/mesto-backend && cd ~/mesto-backend && rm -rf ~/repo && npm i && npm run build',
+      'post-deploy': 'mkdir ~/mesto-backend && cp -Rf ./backend/* ~/mesto-backend && cd ~/mesto-backend && rm -rf ~/repo && npm i && npm run build',
     },
   },
 };
