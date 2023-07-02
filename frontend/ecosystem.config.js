da4182c40ee0187ea96f455ac8e0d49b19e3decc
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2023-07-02 17:23:58 +0500

    +12

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'origin/master',
+  TEMP_PATH = '~/temp',
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'git@github.com:Zorkiy82/web-plus-pm2-deploy.git',
+      path: TEMP_PATH,
+      'post-deploy': `mkdir ${DEPLOY_PATH} && cd ${TEMP_PATH}/frontend && npm i && npm run build && cp -Rf ./* ${DEPLOY_PATH} && rm -rf ${TEMP_PATH}`,
+    },
+  },
+};
