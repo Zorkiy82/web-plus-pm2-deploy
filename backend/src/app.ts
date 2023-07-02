1f65abaf735fb67d1f04290a26f4548cb3afb876
Aleksandr Ogorodnikov <Ogorodnikov.Aaleksandr@yandex.ru>
2023-07-02 15:21:59 +0500

    +5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,9 +4,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
-import { DB_ADDRESS } from './config';
+import { DB_ADDRESS, JWT_SECRET } from './config';
 import routes from './routes';
 
 const { PORT = 3000 } = process.env;
@@ -14,7 +14,7 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -24,5 +24,7 @@ app.use(errorHandler);
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => {
-  console.log('ok');
+  console.log('PORT = ', PORT);
+  console.log('DB_ADDRESS = ', DB_ADDRESS);
+  console.log('JWT_SECRET = ', JWT_SECRET);
 });
